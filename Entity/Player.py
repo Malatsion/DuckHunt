7dc46c2c67945b261970a2dc20126347bda8f00c
Malatsion <m.malaction06@gmail.com>
2025-04-08 22:04:17 +0300

    fix player ammo

--- Entity/Player.py
+++ Entity/Player.py
@@ -1,27 +1,17 @@
 class Player:
+    """Клас гравця, який зберігає інформацію про ім'я та очки"""
+
     def __init__(self, name: str = "Player"):
-        self.name = name
-        self.score = 0
-        self.max_score = 0
-        self.ammo = 3
-        self.ducks_shot = 0
-        self.round = 1
+        self.player_name = name  # Ім'я гравця
+        self.points = 0  # Поточні очки
+        self.max_points = 0  # Максимальні очки за сесію
 
-    def add_score(self, points: int) -> None:
-        """Додає очки до рахунку"""
-        self.score += points
-        if self.score > self.max_score:
-            self.max_score = self.score
+    def add_points(self, amount: int) -> None:
+        """Додає очки гравцю та оновлює максимальний рахунок"""
+        self.points += amount
+        self.check_max_points()
 
-    def use_ammo(self) -> None:
-        """Використовує один патрон"""
-        if self.ammo > 0:
-            self.ammo -= 1
-
-    def reload(self) -> None:
-        """Перезаряджає зброю"""
-        self.ammo = 3
-
-    def duck_shot(self) -> None:
-        """Збільшує лічильник збитих качок"""
-        self.ducks_shot += 1
+    def check_max_points(self) -> None:
+        """Оновлює максимальний рахунок, якщо поточний більший"""
+        if self.points > self.max_points:
+            self.max_points = self.points
